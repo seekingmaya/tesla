3ad9ce6d922dc5011e572449ef29b26c4a1bdc18
qubit88 <qubertec@gmail.com>
2019-05-09 19:04:09 +0300

    add meta tag for initial scale, change model size on tablet

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
+import "@babel/polyfill";
 import "../scss/main.scss";
 import "./vh-fix";
 import "./tesla";
 
 //fix for scroll bounce
-document.body.addEventListener('touchmove', function(e) { e.preventDefault()}, { passive: false });
+document.body.addEventListener(
+  "touchmove",
+  function(e) {
+    e.preventDefault();
+  },
+  { passive: false }
+);
